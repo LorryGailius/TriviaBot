5f62a51faecc408cc1421ec53c0ee000a0b24bb9
Laurynas <lauris.gailius@gmail.com>
2023-04-16 21:30:30 +0300

    Implemented !trivia command

--- Commands/Essential.cs
+++ Commands/Essential.cs
@@ -1,6 +1,8 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.EventHandling;
+using DSharpPlus.Interactivity.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,18 +38,61 @@ namespace TriviaBot.Commands
         {
             QuestionManager questionManager = new QuestionManager();
 
-            int responseCode = await questionManager.GetQuestions();
+            var interactivity = context.Client.GetInteractivity();
 
-            if(responseCode != 0)
+            Dictionary<DiscordUser, int> score = new Dictionary<DiscordUser, int>();
+
+            int responseCode = await questionManager.GetQuestions(5);
+
+            if (responseCode != 0)
             {
                 await context.Channel.SendMessageAsync(":red_square: **ERROR FETCHING QUESTIONS** :red_square:");
                 return;
             }
 
-            foreach(Question question in questionManager.questions)
+            DiscordEmoji accept = DiscordEmoji.FromName(context.Client, ":white_check_mark:", false);
+            TimeSpan timer = TimeSpan.FromSeconds(20);
+            int timeLeft = 10;
+
+            var joinMessage = new DiscordEmbedBuilder()
+            {
+                Title = $"{DiscordEmoji.FromName(context.Client, ":orange_circle:", false)} **DISCORD TRIVIA** {DiscordEmoji.FromName(context.Client, ":orange_circle:", false)}",
+                Description = $"**To join the game please react with {accept}**\n**Good luck and have fun!**\n**Time remaining to join: {timeLeft} seconds!**",
+            };
+
+            var sentInvite = await context.Channel.SendMessageAsync(embed: joinMessage);
+            timeLeft--;
+
+            await sentInvite.CreateReactionAsync(accept);
+
+            for (int i = 0; i <= timeLeft; timeLeft--)
+            {
+                await Task.Delay(1000);
+                var newMessage = new DiscordEmbedBuilder()
+                {
+                    Title = $"{DiscordEmoji.FromName(context.Client, ":orange_circle:", false)} **DISCORD TRIVIA** {DiscordEmoji.FromName(context.Client, ":orange_circle:", false)}",
+                    Description = $"**To join the game please react with {accept}**\n**Good luck and have fun!**\n**Time remaining to join: {timeLeft} seconds!**",
+                };
+                await sentInvite.ModifyAsync(new DiscordMessageBuilder().AddEmbed(newMessage));
+            }
+
+            int count = 0;
+            var joinReaction = await sentInvite.GetReactionsAsync(accept);
+
+            foreach (var user in joinReaction)
+            {
+                if (!user.IsBot)
+                {
+                    count++;
+                    score.Add(user, 0);
+                }
+            }
+
+            foreach (Question question in questionManager.questions)
             {
                 List<string> answers = new List<string>();
                 Random rng = new Random();
+                int time = 10;
 
                 // Randomize answers
                 answers.Add(question.correct_answer);
@@ -59,18 +104,62 @@ namespace TriviaBot.Commands
                                                 DiscordEmoji.FromName(context.Client, ":three:", false),
                                                 DiscordEmoji.FromName(context.Client, ":four:", false),};
 
-                var message = new DiscordEmbedBuilder()
+                var questionMessage = new DiscordEmbedBuilder()
                 {
                     Title = $"{question.question}",
-                    Description = $"{emoji[0]} | {answers[0]}\n\n" + $"{emoji[1]} | {answers[1]}\n\n" + $"{emoji[2]} | {answers[2]}\n\n" + $"{emoji[3]} | {answers[3]}\n\n" + $"",
+                    Description = $"Time remaining: {time} seconds",
                 };
 
-                await context.Channel.SendMessageAsync(embed: message);
+                var discordMessage = await context.Channel.SendMessageAsync(embed: questionMessage);
+                time--;
 
-                await Task.Delay(10000);
+                foreach (DiscordEmoji em in emoji)
+                {
+                    await discordMessage.CreateReactionAsync(em);
+                }
+
+                for (int i = 0; i <= time; time--)
+                {
+                    await Task.Delay(1000);
+                    var newmessage = new DiscordEmbedBuilder()
+                    {
+                        Title = $"{question.question}",
+                        Description = $"{emoji[0]} | {answers[0]}\n\n" + $"{emoji[1]} | {answers[1]}\n\n" + $"{emoji[2]} | {answers[2]}\n\n" + $"{emoji[3]} | {answers[3]}\n\n" + $"Time remaining: {time} seconds",
+                    };
+                    await discordMessage.ModifyAsync(new DiscordMessageBuilder().AddEmbed(newmessage));
+                }
+
+                //Find index with correct answer
+                int correctIdx = answers.IndexOf(question.correct_answer);
+
+                var correctUsers = await discordMessage.GetReactionsAsync(emoji[correctIdx]);
+
+                foreach (var user in correctUsers)
+                {
+                    if (!user.IsBot)
+                    {
+                        score[user] += 1;
+                    }
+                }
+
+                await Task.Delay(1000);
+
+                await context.Channel.SendMessageAsync($"Correct answer was **{question.correct_answer}**");
+
+                await Task.Delay(3000);
             }
+
+            //Find user with highest score
+            var winner = score.Aggregate((l, r) => l.Value > r.Value ? l : r);
+
+            var finalMessage = new DiscordEmbedBuilder()
+            {
+                Title = $"{DiscordEmoji.FromName(context.Client, ":orange_circle:", false)} **DISCORD TRIVIA** {DiscordEmoji.FromName(context.Client, ":orange_circle:", false)}",
+                Description = $"**The game has ended!\nWinner is: {winner.Key.Mention} with a score of {winner.Value}**",
+            };
+
+            await context.Channel.SendMessageAsync(embed: finalMessage);
+
         }
-
-
     }
 }
