b4315043579551539664855da2d0da193d07d5b6
Laurynas <lauris.gailius@gmail.com>
2023-04-16 16:08:14 +0300

    Initializing bot

---
+++ Bot.cs
@@ -0,0 +1,68 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using TriviaBot.Commands;
+
+namespace TriviaBot
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public InteractivityExtension Interactivity { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJSON>(json);
+
+            var config = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+            Client = new DiscordClient(config);
+            Client.UseInteractivity(new InteractivityConfiguration()
+            {
+                Timeout = TimeSpan.FromMinutes(5)
+            });
+
+            var CommandsNextConfiguration = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableMentionPrefix = true,
+                EnableDms = false,
+                EnableDefaultHelp = false,
+            };
+
+            Commands = Client.UseCommandsNext(CommandsNextConfiguration);
+
+            Commands.RegisterCommands<Essential>();
+
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace TriviaBot
     {
         static void Main(string[] args)
         {
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
         }
     }
 }
